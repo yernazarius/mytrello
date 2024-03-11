ac6c6e0b796e95ed96fd079e6ca056aaaf36dc61
yernazarius <ernzaryakupov04@gmail.com>
2024-03-11 21:46:42 +0500

    backend final version

---
+++ my-trello-backend/src/pomodoro/pomodoro.dto.ts
@@ -0,0 +1,17 @@
+import { IsBoolean, IsNumber, IsOptional } from "class-validator";
+
+
+export class PomodoroSessionDto {
+    @IsOptional()
+    @IsBoolean()
+    isCompleted: boolean
+}
+
+export class PomodoroRoundDto {
+    @IsNumber()
+    totalSeconds: number
+
+    @IsOptional()
+    @IsBoolean()
+    isCompleted: boolean
+}
