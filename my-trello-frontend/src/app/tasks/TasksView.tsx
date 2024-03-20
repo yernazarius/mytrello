a4e078c331548b1476f1a3dc75fd10d53441dae1
yernazarius <ernzaryakupov04@gmail.com>
2024-03-21 02:52:19 +0500

    kanban view

--- my-trello-frontend/src/app/tasks/TasksView.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { ListView } from "./list-view/ListView"
-
-
-
-export function TasksView() {
-    return <ListView />
-}
--- my-trello-frontend/src/app/tasks/list-view/ListView.tsx
+++
@@ -1,3 +0,0 @@
-export function ListView() {
-    return <div>ListView</div>
-}
