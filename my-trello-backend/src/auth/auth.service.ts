9d0671e0dd7eb9f24966d10d5645a756611dbde5
yernazarius <ernzaryakupov04@gmail.com>
2024-03-22 03:43:15 +0500

    some changes for deploy

--- my-trello-backend/src/auth/auth.service.ts
+++ my-trello-backend/src/auth/auth.service.ts
@@ -83,7 +83,7 @@ export class AuthService {
             domain: '.onrender.com',
             expires: expiresIn,
             secure: true,
-            sameSite: 'none'
+            sameSite: 'lax'
         })
     }
 
@@ -93,7 +93,7 @@ export class AuthService {
             domain: '.onrender.com',
             expires: new Date(0),
             secure: true,
-            sameSite: 'none'
+            sameSite: 'lax'
         })
     }
 
