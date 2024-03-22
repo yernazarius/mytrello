3d235a359e50bb82f9fa3daa8d26a07a4abd3eed
yernazarius <ernzaryakupov04@gmail.com>
2024-03-22 15:27:20 +0500

    some changes for deploy

--- my-trello-frontend/src/services/auth-token.service.ts
+++ my-trello-frontend/src/services/auth-token.service.ts
@@ -14,7 +14,7 @@ export const saveTokenStorage = (accessToken: string) => {
     Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
         httpOnly: true,
         sameSite: 'lax',
-        domain: 'onrender.com',
+        // domain: 'onrender.com',
         secure: true,
         expires: 1
     });
